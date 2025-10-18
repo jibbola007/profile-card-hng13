24b7ef8a1f0637cd08383e66b53063a4f34dd101
Jibola007 <ajibbola007@gmail.com>
2025-10-18 22:31:51 +0100

    profile card

---
+++ script.js
@@ -0,0 +1,11 @@
+const timeElement = document.getElementById("user-time");
+
+function updateTime() {
+  timeElement.textContent = Date.now();
+}
+
+
+updateTime();
+
+
+setInterval(updateTime, 1000);
